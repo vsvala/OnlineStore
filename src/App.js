a0f7e6c3a45bd8ed663cff58893d36c02f4af235
Virva <virva.svala@gmail.com>
2021-12-20 13:45:59 +0200

    cleaning front

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import Context from "./Context";
 
 
 
-
 export default class APP extends Component{
   constructor(props){
     super(props);
@@ -24,28 +23,22 @@ export default class APP extends Component{
     };   
   }
 
+  //Initiates the product list by getting producst from inventory service and set productstate also gets cartstate from localstore
   async componentDidMount(){
-  console.log("didmount")
-  
   let cart = localStorage.getItem("cart");
-  
+  //TODO try catch
   await axios.get('/api/products').then(response => {
   const products= response.data 
-  console.log("products",products)
-  console.log("this",this.state)
-
   cart = cart? JSON.parse(cart) : {};
-  console.log("after")
   this.setState({ products:products, cart });
-  console.log("set this state to products and casrds",this.state)
 }) 
   }
 
+//Adds product to shopping cart, saves state to localstore and to cart state
   addToCart = cartItem => {
-    console.log("addToChart funktio")
     let cart = this.state.cart;
-    if (cart[cartItem.id]) { //product name
-      cart[cartItem.id].amount += cartItem.amount;//add just amount if allready in cart
+    if (cart[cartItem.id]) {
+      cart[cartItem.id].amount += cartItem.amount;
     } else {
       cart[cartItem.id] = cartItem; 
     }
@@ -53,34 +46,30 @@ export default class APP extends Component{
       cart[cartItem.id].amount = cart[cartItem.id].product.stock;
     }   
     localStorage.setItem("cart", JSON.stringify(cart));
-    console.log(cart, "add to cart() funktio asetetaan cart localstoreeen")
     this.setState({ cart });
-    console.log(this.state.cart, "asetetaan cart state")
-    
   };
+
+// removes item prom product cart
   removeFromCart = cartItemId => {
     let cart = this.state.cart;
     delete cart[cartItemId];
     localStorage.setItem("cart", JSON.stringify(cart));
     this.setState({ cart });
   };
-  
+
+ // clears whole product cart
   clearCart = () => {
-    console.log("clearCart localstore ja state.cart tyhjäys")
     let cart = {};
     localStorage.removeItem("cart");
     this.setState({ cart });
   };
 
-
-// place order or if product out of stock clear cart, update products and notify user
+// Places order or if product out of stock clears cart, updates products and notifies user
 checkout = async () => {
   const cart = this.state.cart;
   const orderList =[]
 
     for (const [key, value] of Object.entries(cart)) {
-      console.log(key, value);
-      console.log(value.product.id, value.amount);
       const productId = value.product.id
       const productName = value.product.name
       const producPrice = value.product.price
@@ -91,51 +80,58 @@ checkout = async () => {
   const order= { 
     products:orderList
   }
-  console.log("Order to sent to service:", order)
   
  try {
  const response= await axios.post( '/api/orders', order)
-    console.log('post response data from or orderservisefront', response.data)
+    //console.log('post response data from or orderservise to front', response.data)
   
     if (response.data.status === "OK") {
-      console.log("POST status OK")
+      //console.log("POST status OK getting products from inventory..")
+      try {
       await axios.get('/api/products').then(response => {
-      console.log("resp data/orders from invenrory",response.data)
+     // console.log("response data, producst from invenrory after order",response.data)
       this.setState({ products:response.data });
-      console.log( "state after",  this.state.products);
+     // console.log( "state after ordering products",  this.state.products);
       this.clearCart();
-      this.setState({ notification:"Thank you for your order,  Order sent succefully" });
+      this.setState({ notification:"Thank you for your order,  Order sent succefully"  });
       setTimeout(() => {
         this.setState({ notification:null });    
       }, 5000)
-    })
+      })
+    } catch (error) {
+     // console.log("something went wrong with getting products state after failed order")
+      this.setState({ notification: "Something went wrong, please refresh the page" });
+      setTimeout(() => {
+        this.setState({ notification:null });    
+      }, 5000)
+    }   
     }
+    
    //If some products are not available, update product state, clear shopping cart and notify user to replace the order 
    if (response.data.status === "NOT_OK"){
-      console.log("POST NOT OK, Tilaus ei onnistunut joitain tuotteita ei satavilla")
       const notAvailable = response.data.products.filter(item=> item.status==="NOT_OK") 
-      notAvailable.forEach(element => console.log(element.name));
       const noproducts=notAvailable.map(s=>s.name)
       this.setState({ notification: `OUT OF STOCK:  ${noproducts}   Please replace the order!!!!`});
-      //notAvailable.forEach(element =>  this.setState({ notification: `OUT OF STOCK: ${element.name}   Please replace the order!!!!`}));
       setTimeout(() => {
         this.setState({ notification:null });    
       }, 15000)
     
-
-
+//fetch the newest product state from inventory service
+  try {
       await axios.get('/api/products').then(response => {
-        console.log("resp data/orders from invenrory",response.data)
         this.setState({ products:response.data });
-        console.log( "state after",  this.state.products);
         this.clearCart();
       })
-    
+      return response.data;
+
+    } catch (error) {
+      this.setState({ notification: "Something went wrong, please refresh the page" });
+      setTimeout(() => {
+        this.setState({ notification:null });    
+      }, 5000)
+   }   
   }
-  else{
-  console.log("Tilaus ei onnistunu something went wrong")
- }
-  return response.data;
+ return response.data;
 
 } catch (error) {
     this.setState({ notification: 'Something went wrong. Please replace the order!.' });
@@ -203,7 +199,6 @@ render() {
             <Routes>
               <Route exact path="/" element={<ProductList/>} />
               <Route exact path="/cart" element={<Cart/>} />
-              {/* <Route exact path="/products" element ={<ProductList/>} /> */}
             </Routes>
           </div>
         </Router>
@@ -212,10 +207,3 @@ render() {
   }
 }
 
-
-
-  /*      const products = this.state.products.map(p => {
-      if (cart[p.name]) {
-        p.stock = p.stock - cart[p.name].amount; //vähentää product stock nimen mukaan tuotteen  
-      } return p;}) */
-     // this.setState({ products })
